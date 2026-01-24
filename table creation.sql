87b92310120cebc2cb45920585295e38b3c8bef4
ADEBAYO <atkinsonbayo01@gmail.com>
2026-01-24 10:15:52 +0000

    Create table creation.sql

---
+++ table creation.sql
@@ -0,0 +1,16 @@
+CREATE TABLE footwear(
+	Order_ID INT PRIMARY KEY,
+	Order_Date DATE,
+	Customer_Name VARCHAR(50),
+	City CHAR(30),
+	State CHAR(15),
+	Region CHAR(6),
+	Country CHAR (15),
+	Category VARCHAR(30),
+	Sub_Category CHAR(15),
+	Product_Name CHAR(30),
+	Quantity INT,
+	Unit_Price FLOAT,
+	Revenue FLOAT,
+	Profit FLOAT
+);
